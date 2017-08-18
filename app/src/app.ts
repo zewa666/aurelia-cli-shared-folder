234c0890467e50a7e8a9e0adf65a6dabdbfb1a7f
vsoftic <vsoftic@ranorex.com>
2017-08-18 14:28:24 +0200

    initial commit

---
+++ app/src/app.ts
@@ -0,0 +1,13 @@
+import { DemoModel, guid } from "../../shared/index";
+
+export class App {
+  public person: DemoModel;
+
+  attached() {
+    this.person = {
+      firstName: "John",
+      lastName: "Rambo",
+      id: guid()
+    };
+  }
+}
---
+++ shared/models.ts
@@ -0,0 +1,5 @@
+export interface DemoModel {
+  firstName: string;
+  lastName: string;
+  id: string;
+}
